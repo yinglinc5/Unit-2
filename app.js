9838e2fd55c1a145b459d852ed6b946a4f7af8dd
yinglinc5 <yinglinc5@nycstudents.net>
2026-09-18 11:01:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const isLoggedIn = true;
+/* const isLoggedIn = true;
 const role = "admin";
 if(isLoggedIn === true){
     if(role === "admin") {
@@ -7,4 +7,12 @@ if(isLoggedIn === true){
         console.log ("need mod privs")
     }
 }
-
+ */
+const x = 0
+function classifier(x){
+    if (x === 0);
+console.log("Zero");}{
+    elseif (x < 0 && x % 2 === 0);
+        console.log("positive even");
+   
+}
