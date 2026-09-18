685d49417d7a54a83abb8491c6cbb6334905fb42
yinglinc5 <yinglinc5@nycstudents.net>
2026-09-18 10:27:38 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const isLoggedIn = true;
+const role = "admin";
+if(isLoggedIn === true){
+    if(role === "admin") {
+        console.log("remove post");
+    } else{
+        console.log ("need mod privs")
+    }
+}
+
